0040fa19eddc02e05d9e6e2bb248c9c6ee67a9e7
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-21 18:41:20 +0500

    making a card

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import CountryCard from './CountryCard'; 
+function App() {
+  return <body>
+    <CountryCard
+    countryName = "Islamic Republic of Pakistan"
+    population = "241,490,000"
+    flagImageUrl = "https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg"
+    region = "Subcontinent"
+    capital = "Islamabad"/>
+    
+    <CountryCard
+    countryName = "Islamic Republic of Pakistan"
+    population = "241,490,000"
+    flagImageUrl = "https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg"
+    region = "Subcontinent"
+    capital = "Islamabad"
+    />
+  </body> 
+}
+
+export default App;
