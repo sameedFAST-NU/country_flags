c94b061366631eb278a3eaef435239190d9fc0be
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-22 18:56:35 +0500

    made header

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="header">
-                <p>Where in the world?</p>
+                <p className="title">Where in the world?</p>
         </div>
     )
 }
