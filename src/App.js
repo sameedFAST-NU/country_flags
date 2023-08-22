cb3efd8111b68f090fffe49daf16e7e50e2a3bab
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-22 16:50:46 +0500

    country data fetched- flag remaining

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import CountryCard from './CountryCard'; 
+import CountryCard from './CountryCard';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://restcountries.com/v3.1/all")
+    .then(response => setCountries(response.data))
+    .catch(error => console.error(error))
+  })
+
   return <body>
     <div className='cardsContainer'>
-    <div className='card'>
-      <CountryCard
-      countryName = "Islamic Republic of Pakistan"
-      population = "241,490,000"
-      flagImageUrl = "https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg"
-      region = "Subcontinent"
-      capital = "Islamabad"/>
+      {
+        countries.map((country) => (
+          <div className='card'>
+          <CountryCard
+          countryName = {country.name.official}
+          population = {country.population}
+          flagImageUrl = "https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg"
+          region = {country.region}
+          capital = {country.capital}
+          />
+        </div>
+        ))
+      }
+    
     </div>
-    <div className='card'>
-      <CountryCard
-      countryName = "Islamic Republic of Pakistan"
-      population = "241,490,000"
-      flagImageUrl = "https://cdn.britannica.com/46/3346-004-D3BDE016/flag-symbolism-Pakistan-design-Islamic.jpg"
-      region = "Subcontinent"
-      capital = "Islamabad"
-      />
-      </div>
-      </div>
   </body> 
 }
 
