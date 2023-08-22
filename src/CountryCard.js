6e4e2bdb420a15372003d00f366191cfb6516435
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-22 13:19:38 +0500

    by mistake

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -1,19 +1,18 @@
 export default function CountryCard(props) {
     const { countryName, flagImageUrl, population, region, capital } = props;
     return(
-        <div className="card">
+        <>
             <img src={flagImageUrl} 
             alt="">
             </img>
 
             <p className="CountryName">{countryName}</p><br/>
             <div className="cardText">
-            
-            <b>Poppulation: </b><p>{population}</p><br/>
-            <b>Region: </b><p>{region}</p><br/>
-            <b>Capital: </b><p>{capital}</p><br/>
+                <b>Poppulation: </b><p>{population}</p><br/>
+                <b>Region: </b><p>{region}</p><br/>
+                <b>Capital: </b><p>{capital}</p><br/>
             </div>
-        </div>
+        </>
     );
 
 }
