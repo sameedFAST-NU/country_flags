c74ee7e1583b3cbd6b98972b5ecaa7e546c73a6a
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-23 12:47:36 +0500

    dark mode button added

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <div className="header">
                 <p className="title">Where in the world?</p>
+                <button>Dark Mode</button>
         </div>
     )
 }
