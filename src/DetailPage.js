bbc600df08f5966a9f76edc90b81431a1ad79d4e
sameedFAST-NU <l191182@lhr.nu.edu.pk>
2023-08-28 18:03:14 +0500

    api fetched

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,39 +1,76 @@
 import Header from "./Header"
 import DetailPageButton from "./DetailPageButton";
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function DetailPage(props) {
-    const {themeChanger, darkMode, countryName, flagImageUrl, population, region, capital } = props;
+    const {themeChanger, darkMode} = props;
+    const [country, setCountry] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://restcountries.com/v3.1/alpha/BE")
+        .then(response => setCountry(response.data[0]))
+        .catch(error => console.error(error))
+    },[])
+    
+    const getNativeName = (native_Name) => {for (const key in native_Name) {
+        return native_Name[key]["common"];
+    }}
+    const native_Name = getNativeName(country.name?.nativeName)
+
+    const getCurrenciesList = (currencies) => {
+        let currenciesList = []
+        for (const key in currencies) {
+            currenciesList.push(currencies[key]["name"])
+        
+        return currenciesList
+    }}
+    const currencies = getCurrenciesList(country.currencies).toString();
+
+    const getLanguagesList = (languages) => {
+        let languagesList = []
+        for (const key in languages) {
+            languagesList.push(languages[key])
+            console.log(languages[key])
+        }    
+        return languagesList
+    }}
+    const languages = getLanguagesList(country.languages).toString();
 
     return(
-        <body className={darkMode ? 'dark': 'light'}>
+        
+        <div className={darkMode ? 'dark': 'light'}>
             <Header changeTheme = {themeChanger} darkMode = {darkMode}/>
         
             <button  className={darkMode ? 'backButton darkElement': 'backButton lightElement'}>
-                <i class="fa-solid fa-arrow-left-long"></i> Back
+                <i className="fa-solid fa-arrow-left-long"></i> Back
             </button>
             
             <div className="detailsContainer">
                 <div className = "bigFlag">
-                    <img src = {flagImageUrl}></img>
+                    <img src = {country.flags?.png} alt={country.flags?.alt}></img>
                 </div>
 
                 <div className="detailsContent">
                     <div className="detail_name">
-                        Belgium
+                        {country.name?.common}
                     </div>
 
                     <div className="contentcards">
                         <div className="contentCard">
-                            <b>Native Name: </b><p>nameddd</p><br/>
-                            <b>Poppulation: </b><p>39082</p><br/>
-                            <b>Region: </b><p>eu</p><br/>
-                            <b>Sub Region: </b><p>west eu</p><br/>
-                            <b>Capital: </b><p>brussels</p><br/>
+                            <b>Native Name: </b><p>{native_Name}</p><br/>
+                            <b>Population: </b><p>{country.population}</p><br/>
+                            <b>Region: </b><p>{country.region}</p><br/>
+                            <b>Sub Region: </b><p>{country.subregion}</p><br/>
+                            <b>Capital: </b><p>{country.capital}</p><br/>
                         </div>
                         <div className="contentCard">
-                            <b>Top Level Domain: </b><p>nameddd</p><br/>
-                            <b>Currencies: </b><p>nameddd</p><br/>
-                            <b>Languages: </b><p>nameddd</p><br/>
+                            <b>Top Level Domain: </b><p>{country.tld}</p><br/>
+                            <b>Currencies: </b>
+                            {currencies}
+
+                            <br/>
+                            <b>Languages: </b><p>{languages}</p><br/>
                         </div>
                     </div>
 
@@ -46,6 +83,6 @@ export default function DetailPage(props) {
                 </div>
             </div>
 
-        </body>
+        </div>
     )
 }
